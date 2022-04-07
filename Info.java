c5203a5132ae4a06c8ac17acc973798010026b39
Shashwat Pal <shashwatpal1021@gmail.com>
2022-04-07 21:49:07 +0530

    Binary search

---
+++ Info.java
@@ -0,0 +1,20 @@
+public class Info {
+   public static void main(String[] args) {
+      int count = 1;
+      int sum = 0;
+      for (; count <= 6; count++) {
+         sum += count;
+         if (sum == 10) {
+            continue;
+         }
+         if (sum > count * 2) {
+            break;
+         }
+      }
+      do{
+         sum-=count;
+      } while (sum > count);
+      System.out.println(sum);
+      }
+   }
+
