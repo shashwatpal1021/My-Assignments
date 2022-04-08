5d4df78a4953c34a5ca4e1d4ab2454548d032951
Shashwat Pal <shashwatpal1021@gmail.com>
2022-04-08 15:02:09 +0530

    sorting

---
+++ Soting/FindAllDuplicates.java
@@ -0,0 +1,31 @@
+package Soting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllDuplicates {
+   public List<Integer> findDuplicates(int[] arr) {
+      int i = 0;
+      while (i < arr.length) {
+         int correct = arr[i] - 1;
+         if (arr[i] != arr[correct]) {
+            swap(arr, i, correct);
+         } else {
+            i++;
+         }
+      }
+      List<Integer> ans = new ArrayList<>();
+      for (int j = 0; j < arr.length; j++) {
+         if (arr[j] != i + 1) {
+            ans.add(arr[j]);
+         }
+      }
+      return ans;
+   }
+   
+   static void swap(int[] arr, int first, int second) {
+      int temp = arr[first];
+      arr[first] = arr[second];
+      arr[second] = temp;
+   }
+}
